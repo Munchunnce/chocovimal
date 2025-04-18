af653944c7d3ea0ff55497e27a796d3c67a29ffe
Vimal <vimalkumarak478@gmail.com>
2025-04-19 01:42:14 +0530

    Initial commit for Vercel deployment

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,20 +1,39 @@
+// import { db } from "@/lib/db/db";
+// import { products } from "@/lib/db/schema";
+// import { eq } from "drizzle-orm";
+
+// export async function GET(request: Response, {params}: {params: {id: string } }){
+//     const id = params.id;
+
+//     try {
+//         const product = await db.select().from(products).where(eq(products.id, Number(id))).limit(1);
+//         if(!product.length){
+//             return Response.json({ message: 'Product not found.'}, { status: 400 });
+//         }
+
+//         return Response.json(product[0]);
+//     } catch  {
+//         return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
+//     }
+    
+// }
+
 import { db } from "@/lib/db/db";
 import { products } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
-export async function GET(request: Request, {params}: {params: {id: string } }){
+export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
     const id = params.id;
 
     try {
         const product = await db.select().from(products).where(eq(products.id, Number(id))).limit(1);
-        if(!product.length){
-            return Response.json({ message: 'Product not found.'}, { status: 400 });
+        if (!product.length) {
+            return new Response(JSON.stringify({ message: 'Product not found.' }), { status: 400 });
         }
 
-        return Response.json(product[0]);
-    } catch  {
-        return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
+        return new Response(JSON.stringify(product[0]), { status: 200 });
+    } catch {
+        return new Response(JSON.stringify({ message: 'Failed to fetch a product' }), { status: 500 });
     }
-    
 }
-
