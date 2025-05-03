4d237331f1adfb3250151ba46c4d32b46963482e
Er. Vimal Kumar Chaudhary <112014356+Munchunnce@users.noreply.github.com>
2025-05-04 04:21:25 +0530

    Update client.ts

--- src/http/client.ts
+++ src/http/client.ts
@@ -2,7 +2,7 @@
 
  export const api = axios.create({
         baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-        withCredentials: true,
+        withCredentials: false,
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
