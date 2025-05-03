b40810a0d5080ced30248cb804ea34c8726f386d
Vimal <vimalkumarak478@gmail.com>
2025-05-03 23:58:43 +0530

    Initial commit for Vercel deployment

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,34 +1,9 @@
-// import { db } from '@/lib/db/db';
-// import { products } from '@/lib/db/schema';
-// import { eq } from 'drizzle-orm';
-
-// export async function GET(request: Request, { params }: { params: { id: string } }) {
-//     const id = params.id;
-
-//     try {
-//         const product = await db
-//             .select()
-//             .from(products)
-//             .where(eq(products.id, Number(id)))
-//             .limit(1);
-
-//         if (!product.length) {
-//             return Response.json({ message: 'Product not found.' }, { status: 400 });
-//         }
-
-//         return Response.json(product[0]);
-//     } catch (err) {
-//         return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
-//     }
-// }
-
-
 import { db } from '@/lib/db/db';
 import { products } from '@/lib/db/schema';
 import { eq } from 'drizzle-orm';
 
-export async function GET(request: Request, context: { params: { id: string } }) {
-    const id = context.params.id;
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const id = params.id;
 
     try {
         const product = await db
@@ -43,6 +18,6 @@ export async function GET(request: Request, context: { params: { id: string } })
 
         return Response.json(product[0]);
     } catch (err) {
-        return Response.json({ message: 'Failed to fetch a product' }, { status: 500 });
+        return Response.json({ message: 'Failed to fetch a product', err }, { status: 500 });
     }
 }
